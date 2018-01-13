0a986deb24a9a0dde0259a44a0ffa03cf0ea438e
Mat Kelcey <matthew.kelcey@gmail.com>
2018-01-13 13:09:56 +1100

    wip re: patch fraction

--- data.py
+++ data.py
@@ -8,31 +8,59 @@ import tensorflow as tf
 import numpy as np
 from label_db import LabelDB
 
-def img_xys_iterator(base_dir, batch_size, img_shape):
+def img_xys_iterator(base_dir, batch_size, patch_fraction):
   dataset = tf.data.Dataset.from_generator(partial(_decode_img_and_lookup_xys,
                                                    base_dir,
                                                    for_training=False,
-                                                   img_shape=img_shape),
+                                                   patch_fraction=patch_fraction),
                                            output_types=(tf.uint8, tf.uint8))
-  dataset = dataset.batch(batch_size).prefetch(2)
+  dataset = dataset.batch(batch_size) #.prefetch(2)
   return dataset.make_one_shot_iterator().get_next()
 
-def _decode_img_and_lookup_xys(base_dir, for_training, img_shape):
+def _decode_img_and_lookup_xys(base_dir, for_training, patch_fraction):
   label_db = LabelDB(check_same_thread=False)
   while True:
     for fname in os.listdir(base_dir):
-      
-      fullname = "%s/%s" % (base_dir, fname)      
-      img = np.array(Image.open(fullname))
-      h, w, _ = img.shape
 
+      # load full size image
+      img = Image.open("%s/%s" % (base_dir, fname))
+      w, h = img.size
+      print "h", h, "w", w
       # TODO: drop use of img_shape completely
       
       # TODO: distort here
   
       # TODO: lookup xys
-      xys = label_db.get_labels(fullname)
-      print("xys", fullname, xys)
+      xys = label_db.get_labels(fname)
+      print("xys", fname, xys)
+
+      # sample patch
+      if patch_fraction >= 1:
+        # decide patch bounds
+        pw, ph = w / patch_fraction, h / patch_fraction
+        x1 = 0 #int(random.random() * (w - pw))
+        y1 = 0 #int(random.random() * (h - ph))
+        x2 = x1 + pw
+        y2 = y1 + ph
+        print("PATCH!", x1, y1, x2, y2)
+        # crop image
+        img = img.crop((x1, y1, x2, y2))
+        print "post crop", img.size
+        # recreate xys
+        patch_xys = []
+        for x, y in xys:
+          # note: x, y as saved in db are for numpy arrays, not PIL images
+          # to deal with pil images we need to swap them
+          print("check", x, y)
+#          x = int(x / patch_fraction)
+#          y = int(y / patch_fraction)
+#          print("remaps to", x, y)
+          if x > x1 and x < x2 and y > y1 and y < y2:
+            print("..in patch as", x-x1, y-y1)
+            patch_xys.append((x-x1, y-y1))
+        xys = patch_xys
+        h, w = ph, pw
+        print "xyx post patching", xys
         
 #      if img_shape == '256':
 #        xys = [(33, 26), (162, 143), (169, 221)]
@@ -44,12 +72,14 @@ def _decode_img_and_lookup_xys(base_dir, for_training, img_shape):
 #        raise Exception("??? %s" % img_shape)
   
       # done
-      yield img, xys_to_bitmap(xys, h, w, rescale=0.5)
+      yield np.array(img), xys_to_bitmap(xys, h, w, rescale=0.5)
 
 def xys_to_bitmap(xys, height, width, rescale=1.0):
+  print ">xys_to_bitmap xys", xys, "height", height, "width", width, "rescale", rescale
   bitmap = np.zeros((int(height*rescale), int(width*rescale), 1), dtype=np.float32)
-#  print "BB", bitmap.shape
+  print "bitmap.shape", bitmap.shape
   for x, y in xys:
+    print "set", int(x*rescale), int(y*rescale)
     bitmap[int(x*rescale), int(y*rescale), 0] = 1.0
   return bitmap
 
@@ -65,18 +95,19 @@ def bitmap_to_pil_image(bitmap):
 if __name__ == "__main__":
   import argparse
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--base-dir', type=str, default="images/sample_originals")
-#  parser.add_argument('--labelled-data-fname', type=str, default="train.jsons")
-#  parser.add_argument('--img-shape', type=str, default="384")
+  parser.add_argument('--image-dir', type=str, default="images/sample_originals")
+  parser.add_argument('--batch-size', type=int, default=16)
+  parser.add_argument('--patch-fraction', type=int, default=1,
+                      help="what fraction of image to use as patch. 1 => no patch")
   opts = parser.parse_args()
   
   from PIL import Image, ImageDraw
 
   sess = tf.Session()
   
-  imgs, xys = img_xys_iterator(base_dir=opts.base_dir,
-                               batch_size=5,
-                               img_shape="not_used") #opts.img_shape)
+  imgs, xys = img_xys_iterator(base_dir=opts.image_dir,
+                               batch_size=opts.batch_size,
+                               patch_fraction=opts.patch_fraction)
   
   img_batch, xys_batch = sess.run([imgs, xys])
   for i, (img, xys) in enumerate(zip(img_batch, xys_batch)):
