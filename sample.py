3b7d2476b22b6019384703446c26efa89b33e82c
Mat Kelcey <matthew.kelcey@gmail.com>
2018-02-07 11:29:59 +1100

    simple sampling from -> to dir helper

---
+++ sample.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import random
+import sys
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--copy', action='store_true', help='whether to move (dft) or copy file')
+parser.add_argument('-n', '--n', type=int, default=10, help='number to sample')
+parser.add_argument('directories', metavar='directory', type=str, nargs=2, help='src_dir dest_dir')
+opts = parser.parse_args()
+print("# opts %s" % opts, file=sys.stderr)
+
+if opts.n <= 0:
+  raise Exception("can only sample +ve number, not %d" % opts.n)
+
+src_directory, dest_directory = opts.directories
+if not os.path.isdir(src_directory):
+  raise Exception("src_directory [%s] doesn't exist" % src_directory)
+if not os.path.isdir(dest_directory):
+  print("# creating destination directory [%s]" % dest_directory, file=sys.stderr)
+  print("mkdir -p %s" % dest_directory)
+
+src_files = os.listdir(src_directory)
+if len(src_files) < opts.n:
+  raise Exception("requested sample of %d files from directory [%s] but there are only %d files there" %
+                  (opts.n, src_directory, len(from_files)))
+
+random.shuffle(src_files)
+for filename in src_files[:opts.n]:
+  print("%s %s/%s %s" % ("cp" if opts.copy else "mv",
+                         src_directory, filename, dest_directory))
