e67fa14fe390b7f909931f93a205385845019f95
Mat Kelcey <matthew.kelcey@gmail.com>
2018-03-12 09:44:30 +1100

    dont use tf.data for predict

--- predict.py
+++ predict.py
@@ -4,7 +4,6 @@
 
 from PIL import Image, ImageDraw
 import argparse
-import data
 import itertools
 from label_db import LabelDB
 import model
@@ -23,17 +22,21 @@ parser.add_argument('--export-pngs', action='store_true')
 parser.add_argument('--base-filter-size', type=int, default=16)
 opts = parser.parse_args()
 
-# test data reader
-test_imgs, test_filenames = data.img_filename_iterator(base_dir=opts.image_dir)
+# feed data through an explicit placeholder to avoid using tf.data
+# which fails on rasp pi with .so problem :/ :/ :/
+imgs = tf.placeholder(dtype=tf.uint8, shape=(1, 1024, 768, 3))
 
+# test data reader
 with tf.variable_scope("train_test_model") as scope:  # clumsy :/
-  model = model.Model(test_imgs,
+  model = model.Model(imgs,
                       is_training=False,
                       use_skip_connections=not opts.no_use_skip_connections,
                       base_filter_size=opts.base_filter_size,
                       use_batch_norm=not opts.no_use_batch_norm)  
 sess = tf.Session()
 model.restore(sess, "ckpts/%s" % opts.run)
+
+tf.train.write_graph(sess.graph_def, ".", "bnn_graph.predict.pb")
 
 if opts.output_label_db:
   db = LabelDB(label_db_file=opts.output_label_db)
@@ -43,36 +46,36 @@ else:
   
 # TODO: make this batched to speed it up for larger runs
 
-for idx in itertools.count():
+for idx, filename in enumerate(os.listdir(opts.image_dir)):
+
+  # load next image (and add dummy batch dimension)
+  img = np.array(Image.open(opts.image_dir+"/"+filename))
+  imgs = np.expand_dims(img, 0)
+
   try:
-    
-    if opts.export_pngs:
-      fn, img, o = sess.run([test_filenames, test_imgs, model.output])
-      img = img[0]
-    else:
-      fn, o = sess.run([test_filenames, model.output])
-      
-    fn = fn[0]
-    prediction = o[0]
+    # run single image through model
+    prediction = sess.run(model.output, feed_dict={model.imgs: imgs})[0]
 
     # calc [(x,y), ...] centroids
     centroids = u.centroids_of_connected_components(prediction, rescale=2.0)
 
-    print("\t".join(map(str, [idx, fn, len(centroids)])))
+    print("\t".join(map(str, [idx, filename, len(centroids)])))
 
+    # export debug images (if requested)
     if opts.export_pngs:
       # turn these into a bitmap
       # recall! centroids are in half res
+      # TODO: for centroids could draw red dots like in label_ui
       h, w, _ = img.shape
       centroids_bitmap = u.bitmap_from_centroids(centroids, h, w)
       # save rgb / bitmap side by side
-#      debug_img = u.side_by_side(rgb=img, bitmap=prediction) #centroids_bitmap)
+#      debug_img = u.side_by_side(rgb=img, bitmap=prediction)
       debug_img = u.side_by_side(rgb=img, bitmap=centroids_bitmap)
       debug_img.save("predict_example_%03d.png" % idx)
 
-    # set new labels
+      # set new labels (if requested)
     if db:
-      db.set_labels(fn, centroids, flip=True)
+      db.set_labels(filename, centroids, flip=True)
   
   except tf.errors.OutOfRangeError:
     # end of iterator
