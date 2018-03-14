715e6d3849b1781033cabe6bf8b340061fee13ac
Mat Kelcey <matthew.kelcey@gmail.com>
2018-03-14 20:42:39 +1100

    call freeze_graph as module code

--- freeze_graph.sh
+++ freeze_graph.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-python3 /usr/local/lib/python3.5/dist-packages/tensorflow/python/tools/freeze_graph.py \
- --clear_devices \
+python3 -m tensorflow.python.tools.freeze_graph \
  --input_graph bnn_graph.predict.pbtxt \
  --input_checkpoint ckpts/r19_bfs4/20180308_193817 \
  --output_node_names train_test_model/output \
