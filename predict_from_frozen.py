baea91af2371a3e86436b2ce1ab30c8cc4ea1bed
Mat Kelcey <matthew.kelcey@gmail.com>
2018-03-15 15:56:56 +1100

    allow specification of graph

--- predict_from_frozen.py
+++ predict_from_frozen.py
@@ -3,18 +3,19 @@
 # given a directory of images output a list of image -> predictions
 
 from PIL import Image, ImageDraw
+from label_db import LabelDB
 import argparse
 import itertools
-from label_db import LabelDB
-import model
 import numpy as np
 import os
 import tensorflow as tf
+import time
 import util as u
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--image-dir', type=str, required=True)
 parser.add_argument('--output-label-db', type=str, default=None, help='if not set dont write label_db')
+parser.add_argument('--graph', type=str, default='bnn_graph.predict.frozen.pb', help='graph.pb to use')
 parser.add_argument('--run', type=str, required=True, help='model')
 parser.add_argument('--no-use-skip-connections', action='store_true')
 parser.add_argument('--no-use-batch-norm', action='store_true')
@@ -24,7 +25,7 @@ opts = parser.parse_args()
 
 # restore frozen graph
 graph_def = tf.GraphDef()
-with open("bnn_graph.predict.frozen.pb", "rb") as f:
+with open(opts.graph, "rb") as f:
   graph_def.ParseFromString(f.read())
 tf.import_graph_def(graph_def, name=None)
 
@@ -47,12 +48,16 @@ for idx, filename in enumerate(os.listdir(opts.image_dir)):
 
   # load next image (and add dummy batch dimension)
   img = np.array(Image.open(opts.image_dir+"/"+filename))
+  original_img = img.copy()
+#  img = img.astype(np.float32) / 255
   imgs = np.expand_dims(img, 0)
 
   try:
     # run single image through model
+    s = time.time()
     prediction = sess.run(model_output, feed_dict={imgs_placeholder: imgs})[0]
-
+    print("predict time", time.time()-s)
+    
     # calc [(x,y), ...] centroids
     centroids = u.centroids_of_connected_components(prediction, rescale=2.0)
 
@@ -63,11 +68,11 @@ for idx, filename in enumerate(os.listdir(opts.image_dir)):
       # turn these into a bitmap
       # recall! centroids are in half res
       # TODO: for centroids could draw red dots like in label_ui
-      h, w, _ = img.shape
+      h, w, _ = original_img.shape
       centroids_bitmap = u.bitmap_from_centroids(centroids, h, w)
       # save rgb / bitmap side by side
-#      debug_img = u.side_by_side(rgb=img, bitmap=prediction)
-      debug_img = u.side_by_side(rgb=img, bitmap=centroids_bitmap)
+#      debug_img = u.side_by_side(rgb=original_img, bitmap=prediction)
+      debug_img = u.side_by_side(rgb=original_img, bitmap=centroids_bitmap)
       debug_img.save("predict_example_%03d.png" % idx)
 
       # set new labels (if requested)
