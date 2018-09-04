eb95ea1267420af135ce2a4fdbcfcad163934f9e
Mat Kelcey <matthew.kelcey@gmail.com>
2018-09-05 06:35:14 +1000

    hacking model to be what would run on NCS

--- model.py
+++ model.py
@@ -21,61 +21,79 @@ class Model(object):
       self.imgs = imgs
       model = imgs
 
+      assert use_skip_connections is False, "--use-skip-connections not going to work with padding=SAME"
+
       dump_shape_and_product_of('input', model)
 
-      e1 = slim.conv2d(model, num_outputs=base_filter_size, kernel_size=3, stride=2, scope='e1')
+      e1 = slim.conv2d(model, num_outputs=base_filter_size, kernel_size=3, stride=2,
+                       padding='VALID', scope='e1')
       if use_batch_norm:
         e1 = slim.batch_norm(e1, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('e1', e1)
 
-      e2 = slim.conv2d(e1, num_outputs=2*base_filter_size, kernel_size=3, stride=2, scope='e2')
+      e2 = slim.conv2d(e1, num_outputs=2*base_filter_size, kernel_size=3, stride=2,
+                       padding='VALID', scope='e2')
       if use_batch_norm:
         e2 = slim.batch_norm(e2, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('e2', e2)
 
-      e3 = slim.conv2d(e2, num_outputs=4*base_filter_size, kernel_size=3, stride=2, scope='e3')
+      e3 = slim.conv2d(e2, num_outputs=4*base_filter_size, kernel_size=3, stride=2,
+                       padding='VALID', scope='e3')
       if use_batch_norm:
         e3 = slim.batch_norm(e3, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('e3', e3)
 
-      e4 = slim.conv2d(e3, num_outputs=8*base_filter_size, kernel_size=3, stride=2, scope='e4')
+      e4 = slim.conv2d(e3, num_outputs=8*base_filter_size, kernel_size=3, stride=2,
+                       padding='VALID', scope='e4')
       if use_batch_norm:
         e4 = slim.batch_norm(e4, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('e4', e4)
 
+      e5 = slim.conv2d(e4, num_outputs=16*base_filter_size, kernel_size=3, stride=2,
+                       padding='VALID', scope='e5')
+      if use_batch_norm:
+        e5 = slim.batch_norm(e5, decay=0.9, is_training=is_training)
+      dump_shape_and_product_of('e5', e5)
+
       # record bottlenecked shape for resizing back
       # this is clumsy, how to do this more directly from tensors / config?
-      _batch_size, h, w, _depth = e4.get_shape().as_list()
+#      _batch_size, h, w, _depth = e4.get_shape().as_list()
 
-      model = tf.image.resize_nearest_neighbor(e4, [h*2, w*2])
-      model = slim.conv2d(model, num_outputs=4*base_filter_size, kernel_size=3, scope='d1')
+#      model = tf.image.resize_nearest_neighbor(e4, [h*2, w*2])
+#      model = slim.conv2d(model, num_outputs=4*base_filter_size, kernel_size=3, scope='d1')
+      model = slim.conv2d_transpose(e5, num_outputs=8*base_filter_size,
+                                    kernel_size=3, stride=2, padding='VALID', scope='d1')
       if use_batch_norm:
         model = slim.batch_norm(model, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('d1', model)
 
-      if use_skip_connections:
-        model = tf.concat([model, e3], axis=3)
-        dump_shape_and_product_of('d1+e3', model)
+#      if use_skip_connections:
+#        model = tf.concat([model, e3], axis=3)
+#        dump_shape_and_product_of('d1+e3', model)
 
-      model = tf.image.resize_nearest_neighbor(model, [h*4, w*4])
-      model = slim.conv2d(model, num_outputs=2*base_filter_size, kernel_size=3, scope='d2')
+#      model = tf.image.resize_nearest_neighbor(model, [h*4, w*4])
+#      model = slim.conv2d(model, num_outputs=2*base_filter_size, kernel_size=3, scope='d2')
+      model = slim.conv2d_transpose(model, num_outputs=4*base_filter_size,
+                                    kernel_size=3, stride=2, padding='VALID', scope='d2')
       if use_batch_norm:
         model = slim.batch_norm(model, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('d2', model)
 
-      if use_skip_connections:
-        model = tf.concat([model, e2], axis=3)
-        dump_shape_and_product_of('d2+e2', model)
+#      if use_skip_connections:
+#        model = tf.concat([model, e2], axis=3)
+#        dump_shape_and_product_of('d2+e2', model)
 
-      model = tf.image.resize_nearest_neighbor(model, [h*8, w*8])
-      model = slim.conv2d(model, num_outputs=base_filter_size, kernel_size=3, scope='d3')
+#      model = tf.image.resize_nearest_neighbor(model, [h*8, w*8])
+#      model = slim.conv2d(model, num_outputs=base_filter_size, kernel_size=3, scope='d3')
+      model = slim.conv2d_transpose(model, num_outputs=2*base_filter_size,
+                                    kernel_size=3, stride=2, padding='VALID', scope='d3')
       if use_batch_norm:
         model = slim.batch_norm(model, decay=0.9, is_training=is_training)
       dump_shape_and_product_of('d3', model)
 
-      if use_skip_connections:
-        model = tf.concat([model, e1], axis=3)
-        dump_shape_and_product_of('d3+e1', model)
+#      if use_skip_connections:
+#        model = tf.concat([model, e1], axis=3)
+#        dump_shape_and_product_of('d3+e1', model)
 
       # at this point we are back to 1/2 res of original, which should be enough
 
@@ -84,7 +102,9 @@ class Model(object):
                                 activation_fn=None)
       dump_shape_and_product_of('logits', self.logits)
 
-      self.output = tf.nn.sigmoid(self.logits, name='output')
+      # NCS hacktastic! sigmoid on 2d image causes read error :/
+      # expect to deal with endless "no output" errors ...
+#      self.output = tf.nn.sigmoid(self.logits, name='output')
 
       self.saver = tf.train.Saver(max_to_keep=100,
                                   keep_checkpoint_every_n_hours=1)
